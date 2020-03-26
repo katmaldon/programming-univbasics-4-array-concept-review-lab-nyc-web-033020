ef78a3eee43832fb58e2ab9b9ab7079923d7a288
Kat Maldon <katharinamaldon@gmail.com>
2020-03-26 18:04:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array[value_to_find]
 end
 
 def find_max_value(array)
@@ -7,5 +7,5 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort[0]
 end
